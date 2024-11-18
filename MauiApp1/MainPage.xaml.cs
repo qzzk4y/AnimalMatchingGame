177beda776551fd87630845033198994b6b70e1d
qzzk4y <julio.gallego@gmail.com>
2024-11-18 19:02:12 +0100

    Agregar archivos de proyecto.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayAgainButton_Clicked(object sender, EventArgs e)
+        {
+            AnimalButtons.IsVisible = true;
+            PlayAgainButton.IsVisible = false;
+
+            List<string> animalEmoji = [
+               "👾","👾",
+               "🐱‍👤","🐱‍👤",
+               "🐹","🐹",
+               "🐷","🐷",
+               "🐮","🐮",
+               "🐸","🐸",
+               "🦄","🦄",
+               "🦍","🦍"
+            ];
+
+            foreach (var button in AnimalButtons.Children.OfType<Button>())
+            {
+                int index = Random.Shared.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                button.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+}
